d18d47f4ae831ade4e9a8989f3fd90e52e7de49b
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2024-07-18 17:32:47 +0000

    learn operators

--- 01_Basics/03_dataTypes_conversion.js
+++ 01_Basics/03_dataTypes_conversion.js
@@ -33,5 +33,49 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ******** Operations *********
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " vaibhav"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+//  to understand below script refer ecma script guidelines.
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3); // dirty code 
+
+// console.log(true);
+// console.log(true+); // gives error
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2 // avoid this type of writting also , bcz its affect readability.
+
+let gameCounter = 100
+// gameCounter++; // postfix
+++gameCounter; // prefix
+console.log(gameCounter);
