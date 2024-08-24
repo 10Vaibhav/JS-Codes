ac4b15a7847e352d9bf7def359b31bba61d038fc
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2024-08-24 12:57:44 +0000

    Js_by_mosh completed

---
+++ Js_by_mosh/Objects.js
@@ -0,0 +1,22 @@
+// Reference type : 
+// Object, Array, Function
+
+//Object
+let person = {
+    name:'Vaibhav',
+    age:20
+};
+
+console.log(person);
+
+// Dot Notation
+person.name = 'Mahajan';
+
+// Bracket Notation
+let selection = 'name';
+person[selection] = 'vaikunth';
+
+console.log(person.name);
+
+
+
---
+++ Js_by_mosh/function.js
@@ -0,0 +1,17 @@
+// Performing a task 
+function greet(name, lastname){
+    console.log("Hello "+ name + " " + lastname)
+}
+
+// greet("Vaibhav"); // calling function
+// greet("vaikunth");
+
+// greet("Vaibhav", "Mahajan");
+
+// Calculating a value
+function square(number){
+    return number * number;
+}
+
+let number = square(2);
+console.log(number);
