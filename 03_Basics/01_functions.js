6ef94675c98ceb5b5c3a89b4059092b9a9820c41
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2024-09-03 10:17:42 +0000

    Add JavaScript function examples and parameter handling
    - Implement sayMyName function to print letters
    - Create addTwoNumbers function for basic arithmetic
    - Add loginUserMessage function with parameter validation
    - Demonstrate function calls and variable assignment
    - Include comments explaining function behavior and concepts

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,42 @@
+function sayMyName(){
+    console.log("V");
+    console.log("A");
+    console.log("I");
+    console.log("B");
+    console.log("H");
+    console.log("A");
+    console.log("V");
+}
+
+// sayMyName() // only name of function means -> refference, with () it's execution.
+
+// function addTwoNumbers(number1,number2){
+//     console.log(number1+number2)
+// };
+
+function addTwoNumbers(number1,number2){
+    // let result = number1+number2
+    // return result
+
+    return number1 + number2
+};
+
+const result = addTwoNumbers(10, 5);
+// console.log("Result: ",result)
+
+function loginUserMessage(username) // to set default value for username we use it as "username = sam" in bracket it set "sam" as default value and if value is given then it overwrite the value on sam.
+{
+    if(!username) //username === undefined
+    { 
+        console.log("Please enter a username.")
+        return 
+    }
+
+    return `${username} just logged in.`
+};
+
+// let output = loginUserMessage("Vaibhav");
+
+let output = loginUserMessage(); // it's return undefined at place of username(if we not checking the value in function).
+
+console.log(output)
