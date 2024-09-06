e614941bc8f8145453c89b93c82b0450d40bae03
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2024-09-06 15:19:19 +0000

    more about the scope and see working of function on the basis of it's declaration .

--- 03_Basics/02_Scope.js
+++ 03_Basics/02_Scope.js
@@ -6,10 +6,53 @@ let a = 300;
 if (true){
     let a = 10
     const b = 20
-    console.log("Inner: ", a); // local scope access.
+    // console.log("Inner: ", a); // local scope access.
 }
 
-console.log(a); // global scope access.
+// console.log(a); // global scope access.
 // console.log(b);
 
 //global scope in node environment and in console is work differently .
+
+
+function one(){
+    const username  = "vaibhav"
+
+    function two(){
+        const website = "YT"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "vaibhav";
+
+    if(username == "vaibhav"){
+        const website = " YT";
+        // console.log(username + website);
+    }
+
+    // console.log(website);
+}
+
+// console.log(username);
+
+// +++++++++++++ Interesting +++++++++++++++
+
+console.log(addone(5)); // it runs.
+
+function addone(num){
+    return num+1;
+}
+
+
+addTwo(5); // it gives array.
+const addTwo = function(num){
+    return num+2;
+}
+
