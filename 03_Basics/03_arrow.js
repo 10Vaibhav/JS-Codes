683902ee08f00f9b53c5c625c24bac33c2528bac
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2024-09-06 16:26:01 +0000

    see the this keyword for functions and learn about arrow function .

--- 03_Basics/03_arrow.js
+++ 03_Basics/03_arrow.js
@@ -13,6 +13,35 @@ const user = {
 // user.username = "sam"
 // user.welcomeMessage()
 
-console.log(this); // it returns empty object. 
+// console.log(this); // it returns empty object. 
 
 // global object in browser is window object.
+
+// function chai(){
+//     let username = "vaibhav"
+//     console.log(this.username); // it gives undefined bcz this use in object.
+// }
+
+// chai()
+
+//arrow funcitons: 
+
+// const chai = () =>{
+//     let username = "vaibhav"
+//     console.log(this.username);
+//     console.log(this);
+// }
+
+// chai()
+
+// const addTwo = (num1,num2) => {
+//     return num1+num2
+// }
+
+// const addTwo = (num1,num2) => num1 + num2
+
+// const addTwo = (num1,num2) => (num1+num2)
+
+const addTwo = (num1,num2) => ({username:"vaibhav"})
+
+console.log(addTwo(3,4));
