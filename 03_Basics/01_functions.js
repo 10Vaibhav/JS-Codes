b3a906233a464d6e1d5a9f2e44daaab23718b1d3
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2024-09-06 14:12:38 +0000

    handle Multiple values using Rest operator and handle Array  and objects in function.

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -37,6 +37,46 @@ function loginUserMessage(username) // to set default value for username we use 
 
 // let output = loginUserMessage("Vaibhav");
 
-let output = loginUserMessage(); // it's return undefined at place of username(if we not checking the value in function).
+// let output = loginUserMessage(); // it's return undefined at place of username(if we not checking the value in function).
 
-console.log(output)
+// console.log(output)
+
+
+
+function calculateCartPrice(val1,val2, ...num1) // here, ... is called rest operator.
+{
+    
+    return num1
+}
+
+// console.log(calculateCartPrice(2,200,400,500));
+
+const user = {
+    username: "vaibhav",
+    price: 199
+}
+
+function handleObject(anyObject){
+
+    console.log(`Username is ${ anyObject.username} and price is ${anyObject.price} `);
+}
+
+// handleObject(user);
+
+handleObject({
+    username: "sam",
+    price:399
+});
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+
+    return getArray[1];
+};
+
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([100,200,300,400]));
+
+
