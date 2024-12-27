900bbebc6f04967b1299a281a606757c3b19e529
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2024-12-27 12:53:10 +0530

    Complete API Authentication

---
+++ API Authentication/index.js
@@ -0,0 +1,63 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com/";
+
+const yourUsername = "Mahajan";
+const yourPassword = "BestFamily";
+const yourAPIKey = "0334b26d-fe2d-435c-b845-873d59a84fa7";
+const yourBearerToken = "cea2c115-69e9-4043-8adf-707649796833";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "API Response." });
+});
+
+app.get("/noAuth", async(req, res) => {
+
+  const request = await axios.get("https://secrets-api.appbrewery.com/random");
+  const content = request.data;
+
+  res.render("index.ejs", {content : JSON.stringify(content)});
+
+});
+
+app.get("/basicAuth", async(req, res) => {
+
+  const request = await axios.get("https://secrets-api.appbrewery.com/all?page=2", {
+    auth: {
+      username : yourUsername,
+      password : yourPassword,
+    }
+  });
+
+  const content = request.data;
+
+  res.render("index.ejs", {content: JSON.stringify(content)});
+
+});
+
+app.get("/apiKey", async(req, res) => {
+
+  const request = await axios.get(`https://secrets-api.appbrewery.com/filter?score=5&apiKey=${yourAPIKey}`)
+  const data = request.data;
+  res.render("index.ejs", {content : JSON.stringify(data)});
+
+});
+
+app.get("/bearerToken", async(req, res) => {
+
+  const request = await axios.get("https://secrets-api.appbrewery.com/secrets/42", {
+    headers: {
+      Authorization: `Bearer ${yourBearerToken}`
+    },
+  });
+  const data = request.data;
+  res.render("index.ejs", {content : JSON.stringify(data)});
+
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
