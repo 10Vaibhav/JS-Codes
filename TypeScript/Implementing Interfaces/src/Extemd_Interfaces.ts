50ef8456f7532a8cce69ed443be5c62989418e55
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-03-09 08:11:10 +0530

    Interface can be extends multiple interfaces and types

--- TypeScript/Implementing Interfaces/src/Extemd_Interfaces.ts
+++ TypeScript/Implementing Interfaces/src/Extemd_Interfaces.ts
@@ -6,3 +6,17 @@ interface Admin extends User11 {
     age: number // one interface can extend another interface.
 }
 
+type A = {
+    name: string;
+}
+
+type B = {
+    age: number
+}
+
+interface X extends A,B {} // you also extend multiple interfaces or types.
+
+const user12:X = {
+    name: "Vaibhav",
+    age:20,
+}
