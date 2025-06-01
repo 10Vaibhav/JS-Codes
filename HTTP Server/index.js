d0f43f4260f0abfe78c233de4d99c6b4901929e3
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-06-02 00:23:59 +0530

    URL handling

--- HTTP Server/index.js
+++ HTTP Server/index.js
@@ -1,14 +1,21 @@
 import http from "http";
 import fs from "fs";
+import url from "url";
 
 const myServer = http.createServer((req, res) => {
+    if(req.url === "/favicon.ico") return res.end();
     const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
+
     fs.appendFile('log.txt', log, (err, data)=> {
         // console.log(req.headers);
-        switch(req.url){
+        switch(myUrl.pathname){
             case "/": res.end("HomePage");
             break
-            case "/about": res.end("I am Vaibhav Mahajan");
+            case "/about":
+                const username = myUrl.query.myname;
+                res.end(`Hi, ${username}`);
             break
             default: res.end("404 Not Found");
         }
